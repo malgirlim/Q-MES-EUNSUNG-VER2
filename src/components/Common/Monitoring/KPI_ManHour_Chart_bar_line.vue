46b6105c44e22260fadffc9634d4b35e6499036b
KoBeomSeok <rhqjatjr300@gmail.com>
2023-06-21 16:47:51 +0900

    230621_ko

--- src/components/Common/Monitoring/KPI_ManHour_Chart_bar_line.vue
+++ src/components/Common/Monitoring/KPI_ManHour_Chart_bar_line.vue
@@ -36,7 +36,20 @@ const props = defineProps<{
 
 const chartData = computed<ChartData>(() => {
   return {
-    labels: props.x_label,
+    labels: props.x_label ?? [
+      "1월",
+      "2월",
+      "3월",
+      "4월",
+      "5월",
+      "6월",
+      "7월",
+      "8월",
+      "9월",
+      "10월",
+      "11월",
+      "12월",
+    ],
     datasets: [
       {
         label: props.dataset1_label ?? "공수",
